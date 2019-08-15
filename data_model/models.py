293efdc016c190924e4740593b6a0213815434ff
RM <RRMoelker@users.noreply.github.com>
2019-08-15 17:36:24 +0200

    Model extended to include annotations

--- data_model/models.py
+++ data_model/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from djchoices import DjangoChoices, ChoiceItem
 
 
 class Example(models.Model):
     reference = models.CharField(max_length=48, unique=True)
+
+    def __str__(self):
+        return self.reference
 
 
 class Feature(models.Model):
@@ -14,8 +18,35 @@ class ImageFeature(Feature):
 
 
 class TextFeature(Feature):
-    remote_url = models.URLField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.body[:80]
 
 
 class Annotation(models.Model):
-    pass
+    example = models.ForeignKey(Example, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'annotation {self.example}, label(s): {self.label_set.all()}'
+
+
+class Label(models.Model):
+    annotation = models.ForeignKey(Annotation, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class MultiLabelChoice(DjangoChoices):
+    # Used for multi class annotations, so label is mutually exclusive. Something is an apple or a pear. Not both.
+    exclusive = ChoiceItem()
+
+    # Used for multi label annotations. A meal might contain carbs, fats, proteins and so on.
+    multiple = ChoiceItem()
+
+
+class TextLabel(Label):
+    name = models.CharField(max_length=128)
+    type = models.CharField(max_length=64, choices=MultiLabelChoice.choices, default=MultiLabelChoice.exclusive)
+    group = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.name} ({self.type})'
